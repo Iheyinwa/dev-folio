9d2bb232cd734be8cc1395e565b64d5d3c0b4ee6
Iheyinwa <caruogu@outlook.com>
2023-03-04 04:24:25 -0800

    route to home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-// import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
-// import Skills from './Pages/Skills';
-// import Portfolio from './Pages/Portfolio';
-// import Contact from './Pages/Contact';
 import './styles/main.css';
 
 export default function App() {
-	return <Home />;
+	return (
+		<Routes>
+			<Route path="/" element={<Home />} />
+		</Routes>
+	);
 }
